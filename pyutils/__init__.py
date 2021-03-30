439351b12f11e15c87f517ed7ed4694669f38952
rfmanz <rafael.arevalo27@gmail.com>
2021-03-29 22:31:17 -0500

    change init

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -1,2 +1,8 @@
-from . import eda, fe, load, mdl, opt
+#from . import eda, fe, load, mdl, opt
+from .load import *
+from .eda import *
+from .fe import *
+from .mdl import *
+from .opt import *
+
 __version__ = '0.1'
