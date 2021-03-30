7af8d4ef0ad2813210d711565c069df0901f4864
rfmanz <rafael.arevalo27@gmail.com>
2021-03-29 21:21:47 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(name='pyutils',
+      version='0.1',
+      description='Basic utilities for data science',
+      url='https://github.com/rfmanz/pyutils',
+      license='MIT',
+      packages=['pyutils'],
+      # package_dir={"": "src"},
+      zip_safe=False)
+
+
+
+
