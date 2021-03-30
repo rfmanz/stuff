5341277e072e68f3294203596d9752471b046489
rfmanz <rafael.arevalo27@gmail.com>
2021-03-29 22:59:40 -0500

    change init

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import find_packages, setup
+# from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(name='pyutils',
       version='0.1',
       description='Basic utilities for data science',
       url='https://github.com/rfmanz/pyutils',
       license='MIT',
-      packages=find_packages(where="pyutils"),
-      package_dir={"": "pyutils"},
-      zip_safe=False)
-
-
+      # packages=find_packages(where="pyutils"),
+      packages=['pyutils'])
+# package_dir={"": "pyutils"},
+# zip_safe=False)
