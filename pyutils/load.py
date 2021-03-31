82efaf619629a00190d4793c9db218a1d63b9cc9
rfmanz <rafael.arevalo27@gmail.com>
2021-03-30 20:53:51 -0500

    load change

--- pyutils/load.py
+++ pyutils/load.py
@@ -4,48 +4,47 @@ import zipfile
 import re
 
 
-def read_data(path_ending_with_filename=None, zip_path=None, method=None):
-    """ Creates df with the names of the files. Either single file 'path_ending_with_filename' or 'zip_path' for zip files.
-    pd.read_csv is default. Method option ='dt' for reading with datatable. TODO: expand read methods
-
-    do this with a dictionary:
-    zip_path='/home/r/Downloads/house-prices-advanced-regression-techniques.zip'
-    zf = zipfile.ZipFile(zip_path)
-    files = zf.namelist()
-    d={}
-    for x in files[1:]:
-    d["{0}".format(x)] = pd.read_csv(zf.open(x))
-    train = d['train.csv']
-
+def read_data(path_ending_with_filename=None, return_df=False, zip_path=None, method=None):
+    """
+    Read single csv file or in zip.
+    Available methods:
+        'dt' = Datatable fread
+    TODO: Add to read methods. i.e., parquet, pickle, arrow, etc.
     """
     if path_ending_with_filename:
         fl = path_ending_with_filename.lower()
     else:
         fl = zip_path.lower()
 
-    csv_files = []
     if fl.endswith('.zip'):
-        print('unzipping...')
+        zip_path = '/home/r/Downloads/house-prices-advanced-regression-techniques.zip'
         zf = zipfile.ZipFile(zip_path)
         files = zf.namelist()
-        print(files)
-        for i in files:
-            if i.endswith('.csv'):
-                to_read = i.split('.')[0]
-                csv_files.append(to_read)
-        for i in csv_files:
-            if method == 'dt':
-                globals()[f"{i}"] = dt.fread(zf.open(i + '.csv'))
-                globals()[f"{i}"] = globals()[f"{i}"].to_pandas()
-
-            else:
-                globals()[f"{i}"] = pd.read_csv(zf.open(i + '.csv'))
-                print("Variable created:", f"{i}")
+        dfs = {}
+        for x in files:
+            if x.endswith('.csv'):
+                dfs["{0}".format(re.findall("\w+(?=\.)", x)[0])] = pd.read_csv(zf.open(x))
+        keys = list(dfs.keys())
+        values = list(dfs.values())
+        if return_df:
+            return dfs.values()
+        else:
+            for i in enumerate(dfs):
+                print(i[1], ":", values[i[0]].shape)
+            # print("---" * 18)
+            # print("Copy to assign ↓ (.i.e train,test = read_data(...))")
+            # print("---" * 18)
+            print(str(",".join(keys)))
     else:
         df_name = re.findall("\w+(?=\.)", path_ending_with_filename)[0]
         if method == 'dt':
-            globals()[f"{df_name}"] = dt.fread(path_ending_with_filename)
-            globals()[f"{df_name}"] = globals()[f"{df_name}"].to_pands()
+            df = dt.fread(path_ending_with_filename)
+            df = df.to_pandas()
         else:
-            globals()[f"{df_name}"] = pd.read_csv(path_ending_with_filename)
-        print("Variable created:", f"{df_name}")
+            df = pd.read_csv(path_ending_with_filename)
+        if return_df:
+            return df
+        else:
+            print(df_name, df.shape)
+
+
