dfd61ff7730f0975f7f77756e81a763817b3cf87
rfmanz <rafael.arevalo27@gmail.com>
2021-04-09 12:54:10 -0500

    new example

--- examples/Tabular Playground Series April 2021.py
+++ examples/Tabular Playground Series April 2021.py
@@ -1,5 +1,5 @@
 from pyutils import *
-
+from plotnine import *
 # import pandas as pd
 # import numpy as np
 # from sklearn.preprocessing import *
@@ -27,16 +27,22 @@ train[(train.Age < 1)].value_counts('Survived', normalize=True)
 pd.cut(train.loc[train.Age < 1, 'Age'] * 12, bins=5, right=True).value_counts(normalize=True).sort_index() * 100
 pd.cut(train.loc[train.Age < 1, 'Age'], bins=5, right=True).value_counts().sort_index()
 pd.cut(train.loc[train.Age < 1, 'Age']*12, bins=5, right=True).value_counts().sort_index()
+train[train.Age.between(0.92, 1, inclusive = False)]
 #Babies under the age of 1 who died
 train.loc[(train.Age < 1) & (train.Survived ==0 )]
+#Babies under the age of 1 who died by class
+train.loc[(train.Age < 1) & (train.Survived ==0 )].value_counts(train.Pclass).sum()
+#Dead by class global
+train[train.Survived==0].value_counts(train.Pclass,normalize=True).sort_index()*100
+#we got a 30& reduction in mortality by not being in the lowest class.
+#Mortlity by embarked
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-train[train.Age.between(0.92, 1, inclusive = False)]
+fig, ax = plt.subplots(figsize=(12,6))
+sns.countplot(data=train, x='Embarked', hue='Survived', ax=ax)
 
-train.
-
-pd.cut(train.loc[(train.Parch==0) & (train.Age<=12) & (train.Survived == 0),'Age'],bins=5, right=True).value_counts(normalize=True).sort_index()*100
-
-
+train.loc[:,['Embarked','Survived']].value_counts().unstack().plot.bar(figsize=(12,6),rot=0)
 
 # sibsp = # of siblings / spouses aboard the Titanic
 # parch =  # of parents / children aboard the Titanic
@@ -58,3 +64,6 @@ pd.cut(train.loc[(train.Parch==0) & (train.Age<=12) & (train.Survived == 0),'Age
 # Parent = mother, father
 # Child = daughter, son, stepdaughter, stepson
 # Some children travelled only with a nanny, therefore parch=0 for them.
+
+# ticket	Ticket number
+# fare	Passenger fare
