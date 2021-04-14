5dc00148ad657cd93b5cec3ed3e88d635d888106
rfmanz <rafael.arevalo27@gmail.com>
2021-04-13 19:43:12 -0500

    s

--- examples/Tabular Playground Series April 2021_r.R
+++ examples/Tabular Playground Series April 2021_r.R
@@ -18,17 +18,26 @@ dim(sample_submission)
 DataExplorer::plot_missing(train)
 train[,.N,Sex]
 train[,percent(.N/nrow(train)),.(Survived,Sex)][order(Survived)]
-#Distribution of survived vs not survived of men by age ranges 
+
+#age range men 
+na.omit(train[Sex=='male', .N,.(Hmisc::cut2(Age,c(18,25,50,75)),Sex)])[order(-N), c(.SD, .(Pct =percent(N/na.omit(train[,.N]))))][,c(2,1,3,4)]
+
+
+#Distribution of survived vs not survived of men by age ranges | total percent 
 na.omit(train[Sex=='male',.N,.(Age=Hmisc::cut2(Age,c(18,25,50,75)),Survived)])[order(-N), c(.SD,.(age_group_pct = percent(N/nrow(na.omit(train)))))][order(Age)]
 
-na.omit(train[Sex=='male',.N,.(Age=Hmisc::cut2(Age,c(18,25,50,75)),Survived)])[order(Age),.GRP,Age]
+#Distribution of survived vs not survived of men by age ranges | age group percent  
+na.omit(train[Sex=='male',
+              {GRP = .GRP-1
+              .SD[,.(.N,GRP),Survived]},.(Age=Hmisc::cut2(Age,c(18,25,50,75)))][,c(.SD, .(pct = percent(N/sum(N)), Sex = "Male")),GRP][,-"GRP"][,c(5,seq(1,4))][order(Age,Survived)])
 
-na.omit(train[Sex=='male',.(.N,.GRP),.(Age=Hmisc::cut2(Age,c(18,25,50,75)))])
+hist(train[Sex=='male',.N])
+
+na.omit(train[Sex=='female',
+              {GRP = .GRP-1
+              .SD[,.(.N,GRP),Survived]},.(Age=Hmisc::cut2(Age,c(18,25,50,75)))][,c(.SD, .(pct = percent(N/sum(N)), Sex = "female")),GRP][,-"GRP"][,c(5,seq(1,4))][order(Age,Survived)])
 
 
-
-
-na.omit(train[Sex=='male', .N,.(Hmisc::cut2(Age,c(18,25,50,75)),Sex)])[order(-N), c(.SD, .(Pct =percent(N/na.omit(train[,.N]))))][,c(2,1,3,4)]
 
 #25 to 50 age range survival rate
 
@@ -45,6 +54,8 @@ na.omit(train[Sex=='male' & Survived ==1 , .N, Hmisc::cut2(Age,c(18,25,50,75))])
 
 
 
-
+na.omit(train[,.N,.(Sex, Survived)])
 
 Base_pol[,prop.table(table(is.na(FECANUL)))]
+
+print(t[,.(names = names(t), p = lapply(.SD, function(x) sum(x==0)), t= lapply(.SD, function(x) scales::percent(sum(x==0)/nrow(t))))][order(as.numeric(p)),],topn=150) 
