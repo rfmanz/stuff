43a9141e7314d672a24d61a6a7c22418f0d1f875
rfmanz <42604382+rfmanz@users.noreply.github.com>
2022-01-01 14:23:01 -0500

    Update load.py

--- pyutils/load.py
+++ pyutils/load.py
@@ -37,11 +37,17 @@ def get_csv_names_from_list(paths):
 
 def read_data(path_ending_with_filename=None, return_df=False, method=None, dataframes=None):
     """
+    
+    e.g.
+    ----------------------------------------------------------
+    read_data(path)
+    sample_submission, test, train = read_data(path, True)
+    ----------------------------------------------------------
     Reads single csv or list of csvs or csvs in zip.
 
     Available methods:
         'dt' = Datatable fread
-
+    
     TODO: Add to read methods. i.e., parquet, pickle, arrow, etc.
     """
     dt.options.progress.enabled = True
